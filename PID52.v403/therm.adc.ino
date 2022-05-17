fdad4c8342583dd2d242369a604629a029d0b827
Zhijie Wang <zjwa127@gmail.com>
2022-05-18 00:22:25 +0800

    Update therm.adc.ino

--- PID52.v403/therm.adc.ino
+++ PID52.v403/therm.adc.ino
@@ -30,58 +30,16 @@ void Thermostat() {
 }
 
 void READadc1115() {
-  const int numReadings = 10;
-  int readings1[numReadings];
-  int readings2[numReadings];
-  int readings3[numReadings];
-
-  int readIndex = 0;
-  long total1 = 0;
-  long total2 = 0;
-  long total3 = 0;
 
   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
 
-  float adc0, adc1, adc2, adc3;
+  int16_t adc0, adc1, adc2, adc3;
   delay(30); //settle time
   adc0 = ads.readADC_SingleEnded(0); delay(30);   // this isnt REALLY needed,but it give some time to settle
-  //adc1 = ads.readADC_SingleEnded(1); delay(30);
-  //  adc2 = ads.readADC_SingleEnded(2); delay(30);
-  //  adc3 = ads.readADC_SingleEnded(3); delay(30);
+  adc1 = ads.readADC_SingleEnded(1); delay(30);
+  adc2 = ads.readADC_SingleEnded(2); delay(30);
+  adc3 = ads.readADC_SingleEnded(3); delay(30);
 
-
-
-  for (int i = 0; i < numReadings; i++) {
-    // subtract the last reading:
-    total1 = total1 - readings1[readIndex];
-    total2 = total2 - readings2[readIndex];
-    total3 = total3 - readings3[readIndex];
-
-    // read from the SENSOR1:
-
-    readings1[readIndex] = ads.readADC_SingleEnded(1);
-    readings2[readIndex] = ads.readADC_SingleEnded(2);
-    readings3[readIndex] = ads.readADC_SingleEnded(3);
-
-    // add the reading to the total:
-    total1 = total1 + readings1[readIndex];
-    total2 = total2 + readings2[readIndex];
-    total3 = total3 + readings3[readIndex];
-
-    // advance to the next position in the array:
-    readIndex = readIndex + 1;
-
-    // if we're at the end of the array...
-    if (readIndex >= numReadings) {
-      // ...wrap around to the beginning:
-      readIndex = 0;
-    }
-  }
-
-  adc1 = total1 / numReadings;
-  adc2 = total2 / numReadings;
-  adc3 = total3 / numReadings;
-  Serial.printf("adc1: %.2f. adc2: %.2f. adc3: %.2f. \n", adc1, adc2, adc3);
 
   /////////// sleazy sleazy sleazy bradshaw and bret.....  adc2 is bias is .24 which is 137v .   adc0 is 3.3v power (freindly!)
   /////////// sleazy sleazy sleazy bradshaw and bret.....  adc3 is HV   is .39 which is 375v
@@ -90,19 +48,48 @@ void READadc1115() {
   float a = 0.00128;
   float b = -10.3697;
   signalin = (float)(adc1) * a + b;
-  if (adc2 > 100 and adc2 < 500) {
+  if(adc2 >100 and adc2<500){
     BIASVOLTAGE = 124.6;
-  } else if (adc2 > 500) {
+  }else if(adc2>500){
     BIASVOLTAGE = 75;
-  } else {
+  }else{
     BIASVOLTAGE = 0;
   }
-  if (adc3 > 4000 and adc3 < 6000) {
+  if(adc3 >4000 and adc3<6000){
     HIGHVOLTAGE = -1114;
-  } else if (adc3 > 6000) {
+  }else if(adc3>6000){
     HIGHVOLTAGE = -510;
-  } else {
+  }else{
     HIGHVOLTAGE = 0;
   }  //  //signalin = 6.66666 * signalin;
+
+  //  BIASVOLTAGE = (136 * adc2) / 185;
+  //  HIGHVOLTAGE = (376 * adc3) / 7840;
+  //
+  //  //FUDGE FIX, from Bret's readings . HV . 371 on screen is really 540
+  //  //FUDGE FIX                         Bias 208 on screen is really 122
+  //
+  //  HIGHVOLTAGE = HIGHVOLTAGE * 540. / 367.;
+  //  BIASVOLTAGE = BIASVOLTAGE * 122. / 208.;
+  //
+  //  // SLEAZEBALL make readings zero if they are very low!  Bradshaw 7/19/19
+  //  // FUDGE per Jack.
+  //
+  //  HIGHVOLTAGE = HIGHVOLTAGE * .812;
+//  signalin = (float)(adc1);
+//  BIASVOLTAGE = (float)(adc2);
+//  HIGHVOLTAGE = (float)(adc3);
+//  Serial.println("signalin:");
+//  Serial.println(signalin);
+//  Serial.println("bias V:");
+//  Serial.println(adc2);
+//  Serial.println("high V:");
+//  Serial.println(adc3);
+
+
+  //if (HIGHVOLTAGE < 95.) HIGHVOLTAGE = 0.;
+  //if (BIASVOLTAGE < 20. ) BIASVOLTAGE = 0.;
+
+  //FUDGE FIX . END OF FUDGEFIX
 
 }
