1fd0b21673913d939c71b2cb41aaaa1a7451e511
Zhijie Wang <zjwa127@gmail.com>
2022-05-19 02:38:13 +0800

    fix on 30sec delay after autozero.

--- PID52.v404/justheatcheck.ino
+++ PID52.v404/justheatcheck.ino
@@ -10,15 +10,15 @@ void JUSTheatcheck() {
 
   rangebufdisplay[0] = 0;
   sprintf(rangebufdisplay, "%s", rangeitems[rangein]); // shorten range to include runtime
-//sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
+  //sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
 
   rangebufdisplay[0] = 0;
   Serial.printf("\t\t$$$  range is %s\n\r", rangebufdisplay);
 
-Serial.println("zw readadc started");
+  Serial.println("zw readadc started");
   READadc1115();// is this too often?
   Serial.println("zw readadc finished");
-Serial.println("zw thermostat started");
+  Serial.println("zw thermostat started");
 
   Thermostat();//  is this too often?
   Serial.println("zw thermostat finished");
@@ -27,7 +27,7 @@ Serial.println("zw thermostat started");
 
   delay(500);
 
-starttime=millis();
+  starttime = millis();
 
   while (1) {
 
@@ -64,26 +64,26 @@ starttime=millis();
 
 
     //  bbl going to add in runtime to the range display, sleazy, but quick, and less hours and $$$$$ to QA test the code
-    runsecs=((millis()-starttime)/1000)%60;
-    runmins=(millis()-starttime)/60000;
-    
-    
-    sprintf(rangebufdisplay, "% R = %s",rangeitems[rangein]); // bbl shortened range to inculde runtime
+    runsecs = ((millis() - starttime) / 1000) % 60;
+    runmins = (millis() - starttime) / 60000;
+
+
+    sprintf(rangebufdisplay, "% R = %s", rangeitems[rangein]); // bbl shortened range to inculde runtime
     //sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
 
     //////////////////
 
     Serial.printf("\t\t$$$  range is %s\n\r", rangebufdisplay);
     //Serial.println("zw start highvoltage _ jstheat");
-//    if (HIGHVOLTAGE < 50. )
-//      sprintf(HVbuf, "HV *out*", (float)HIGHVOLTAGE);     // bias and high voltages were swapped  7/12/2020 bbl
-//    else                                                  // bradshaw, bad bradshaw, just swapped the display
-      sprintf(HVbuf, "HV = %.0f V  % d s", (float)HIGHVOLTAGE, (30-(millis()-starttime)/1000));  //  however, this change requires no QA retest, nor big change
-                                                          //  so it is a 1 hour changed, rather than a 6 hour $$$$ change. my bad
-//    if (BIASVOLTAGE < 20.)
-//      sprintf(BIASbuf, "Bias *out*", (float)BIASVOLTAGE);
-//    else
-      sprintf(BIASbuf, "Bias = %.0f V", (float)BIASVOLTAGE);
+    //    if (HIGHVOLTAGE < 50. )
+    //      sprintf(HVbuf, "HV *out*", (float)HIGHVOLTAGE);     // bias and high voltages were swapped  7/12/2020 bbl
+    //    else                                                  // bradshaw, bad bradshaw, just swapped the display
+    sprintf(HVbuf, "HV = %.0f V  % d s", (float)HIGHVOLTAGE, (30 - (millis() - starttime) / 1000)); //  however, this change requires no QA retest, nor big change
+    //  so it is a 1 hour changed, rather than a 6 hour $$$$ change. my bad
+    //    if (BIASVOLTAGE < 20.)
+    //      sprintf(BIASbuf, "Bias *out*", (float)BIASVOLTAGE);
+    //    else
+    sprintf(BIASbuf, "Bias = %.0f V", (float)BIASVOLTAGE);
 
     oledPrettyScreen(HVbuf, BIASbuf , temperbuf, 10, rangebufdisplay, "*heating*");
     if (buttonPushed(15) == 1) {
@@ -99,17 +99,17 @@ starttime=millis();
     }
     delay(250);
     rangebufdisplay[0] = 0;
-//  sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
+    //  sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
     sprintf(rangebufdisplay, "%s", rangeitems[rangein]);// bbl shorten range to include runtime
 
     //////////////////
 
 
     //  bbl going to add in runtime to the range display, sleazy, but quick, and less hours and $$$$$ to QA test the code
-    runsecs=((millis()-starttime)/1000)%60;
-    runmins=(millis()-starttime)/60000;
-    
-    
+    runsecs = ((millis() - starttime) / 1000) % 60;
+    runmins = (millis() - starttime) / 60000;
+
+
     sprintf(rangebufdisplay, " R = %s", rangeitems[rangein]); // bbl shortened range to inculde runtime
     //sprintf(rangebufdisplay, "Range = %s", rangeitems[rangein]);
 
@@ -120,7 +120,7 @@ starttime=millis();
       delay(1000);
       break;
     }
-    if (buttonPushed(15) == 3 or millis() - starttime > 30000) {
+    if (buttonPushed(15) == 3 or millis() - starttime > 2000) {
       menunumber = 0;
       display.clear(); display.display(); Serial.printf("\t\treturn return return\n\n");
       delay(1000);
