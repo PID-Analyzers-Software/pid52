511859779c03df6b069d532b2cf1adec7ac63b7b
Zhijie Wang <zjwa127@gmail.com>
2023-05-04 22:05:02 +0800

    Update OLEDstuff.ino

--- PID52.v407/OLEDstuff.ino
+++ PID52.v407/OLEDstuff.ino
@@ -129,8 +129,8 @@ void  oledPrettyScreen(char *HV, char *BIAS, char *MESSAGE, int font, char *rang
   display.flipScreenVertically();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
-//  sprintf(pbuf, "%s  %s\n\r", HV, BIAS);
-//  display.drawString(0, 5, pbuf);
+  sprintf(pbuf, "%s  %s\n\r", HV, BIAS);
+  display.drawString(0, 5, pbuf);
   display.setFont(ArialMT_Plain_10);
   if (16 == font) display.setFont(ArialMT_Plain_16);
   if (24 == font ) {
